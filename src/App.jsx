bb33c44515084558f243495f9ab703b69d3f2f78
manuelsanchez2 <manusansan22@gmail.com>
2023-04-04 20:50:52 +0200

    🆕 feat: Add jumbotron and refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Jumbotron from "./components/Jumbotron";
 import Nav from "./components/Nav";
 
 export default function App() {
   return (
     <div className="App">
       <Nav />
+      <Jumbotron />
     </div>
   );
 }
